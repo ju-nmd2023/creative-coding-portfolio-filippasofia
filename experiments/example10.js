a6ada8cc53ecda53d2838c9bb47e6453b9120907
filippasofia <fufi23pw@student.ju.se>
2025-09-22 11:17:18 +0200

    new flow field - will add tone.js to it

---
+++ experiments/example10.js
@@ -0,0 +1,55 @@
+let lines = [];
+
+function setup() {
+  createCanvas(650, 650);
+  background(255); 
+
+  for (let i = 0; i < 300; i++) {
+    lines.push(new FlowLine(random(width), random(height)));
+  }
+}
+
+function draw() {
+  for (let l of lines) {
+    l.update();
+    l.show();
+  }
+}
+
+class FlowLine {
+  constructor(x, y) {
+    this.pos = createVector(x, y);
+    this.prev = this.pos.copy(); 
+  }
+
+  update() {
+    this.prev.set(this.pos);
+
+    let angle = noise(
+      this.pos.x * 0.002,
+      this.pos.y * 0.002,
+      frameCount * 0.002
+    ) * TWO_PI * 2;
+
+    let step = p5.Vector.fromAngle(angle);
+    step.setMag(0.3);
+    this.pos.add(step);
+
+    
+    if (
+      this.pos.x < 0 ||
+      this.pos.x > width ||
+      this.pos.y < 0 ||
+      this.pos.y > height
+    ) {
+      this.pos = createVector(random(width), random(height));
+      this.prev.set(this.pos);
+    }
+  }
+
+  show() {
+    stroke(0);        
+    strokeWeight(1.5);  
+    line(this.prev.x, this.prev.y, this.pos.x, this.pos.y);
+  }
+}
